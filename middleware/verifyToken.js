51ed2fc72bed9578e6fec8da3329a3a9b8ec644a
Josip Prpić <josippr@gmail.com>
2025-04-14 18:25:45 +0200

    add verifyToken middleware (#2)

---
+++ middleware/verifyToken.js
@@ -0,0 +1,22 @@
+
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
+module.exports = verifyToken;
