bd9fb618c4579bde142024091b9f23cf268f1407
Josip Prpić <josippr@gmail.com>
2025-04-19 11:44:22 +0200

    bugfix - fix db name bug which duplicated db name

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const app = express();
 const port = process.env.PORT;
 
 const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI_ORIGINAL = process.env.MONGO_URI_ORIGINAL;
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
 const AUTH_SOURCE = process.env.AUTH_SOURCE;
 
 // mongoose.set('debug', true);
-mongoose.connect(MONGO_URI + MONGO_DB_NAME + AUTH_SOURCE)
+mongoose.connect(MONGO_URI_ORIGINAL + MONGO_DB_NAME + AUTH_SOURCE)
   .then(() => console.log("MongoDB connected!"))
   .catch(err => console.error("MongoDB connection error:", err));
 
