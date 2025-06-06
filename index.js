83912a3261ddb07d28c74b0b31c79fdeec8dd2e0
Josip Prpić <josippr@gmail.com>
2025-06-06 15:46:27 +0200

    add experimental metrics api

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const data = require('./api/data/data');
 const registerDevice = require('./api/register-device/register-device');
 const network = require('./api/network/network');
 const usageMetrics = require('./api/logs/usage-metrics');
+const metrics = require('./api/metrics/metrics');
 
 const app = express();
 const port = process.env.PORT;
@@ -41,6 +42,7 @@ app.use('/api/data', data);
 app.use('/api/register-device', registerDevice, limiter);
 app.use('/api/network', network);
 app.use('/api/logs', usageMetrics);
+app.use('/api/metrics', metrics);
 
 app.use(router);
 
